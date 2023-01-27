378841b7ba3986b7e80c5c4cbaf9867bdf939e01
Raaydown <111608769+raaydown@users.noreply.github.com>
2023-01-27 23:33:35 +0100

    j7y17lte: fully Rework bc.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,57 +1,21 @@
-#
-# Copyright (C) 2023 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 DEVICE_PATH := device/samsung/j7y17lte
 
-# Architecture
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 := 
-TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+# Board
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
-
-# APEX
-OVERRIDE_TARGET_FLATTEN_APEX := true
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := universal7870
-TARGET_NO_BOOTLOADER := true
-
-# Display
-TARGET_SCREEN_DENSITY := 420
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_j7y17lte
 
 # Kernel
+BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_CONFIG := exynos7870-j7y17lte_defconfig
 
 # Partitions
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_BOOTIMAGE_PARTITION_SIZE := 20660240
-
-# Platform
-TARGET_BOARD_PLATFORM := exynos5
-
-# Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2571279104
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 39845888
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.samsungexynos7870
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-08-01
@@ -61,3 +25,6 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Inherit the proprietary files
 include vendor/samsung/j7y17lte/BoardConfigVendor.mk
+
+# Inherit common board flags
+include device/samsung/universal7870-common/BoardConfigCommon.mk
