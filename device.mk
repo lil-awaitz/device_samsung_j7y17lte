f34f6ee8bdf18fde3b3460cafd7d77984975aaeb
Raaydown <111608769+raaydown@users.noreply.github.com>
2023-01-27 23:41:47 +0100

    j7y17lte: Rework device.mk

--- device.mk
+++ device.mk
@@ -4,31 +4,52 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Health
+# Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@3.0-impl \
+    libkeymaster3device
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/android.hardware.keymaster@3.0-service.xml:$(TARGET_COPY_OUT_VENDOR)/etc/vintf/manifest/android.hardware.keymaster@3.0-service.xml
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+		frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml
 
 # Overlays
-PRODUCT_ENFORCE_RRO_TARGETS := *
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay
 
 # Product characteristics
 PRODUCT_CHARACTERISTICS := default
 
 # Rootdir
 PRODUCT_PACKAGES += \
-    install-recovery.sh \
+	fstab.samsungexynos7870 \
+	init.target.rc \
+	init.baseband.rc
 
-PRODUCT_PACKAGES += \
-    fstab.samsungexynos7870 \
+# Inherit from common (audio)
+$(call inherit-product, device/samsung/universal7870-common/device-oss_audio.mk)
 
-# Shipping API level
-PRODUCT_SHIPPING_API_LEVEL := 24
+# Custom mixer_paths prebuilt
+#PRODUCT_COPY_FILES += \
+#    $(DEVICE_PATH)/configs/audio/prebuilt/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+#    $(DEVICE_PATH)/configs/audio/prebuilt/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml
 
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+# Inherit from common
+# $(call inherit-product, device/samsung/universal7870-common/device-prebuilt_audio.mk)
 
-# Inherit the proprietary files
-$(call inherit-product, vendor/samsung/j7y17lte/j7y17lte-vendor.mk)
+# Properties
+-include $(DEVICE_PATH)/system_prop.mk
+
+# Inherit from common
+$(call inherit-product, device/samsung/universal7870-common/device-common.mk)
+
+# $(call inherit-product, device/samsung/universal7870-common/device-prebuilt_bsp-vndk.mk)
+
+$(call inherit-product-if-exists, vendor/samsung/j7y17lte/j7y17lte-vendor.mk)
